dab8050c690a02bb44ee0fbb02f45b0b91d6cbec
dmathias <david.mathias@nokia.com>
2018-02-17 09:51:30 +0530

    backup

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ exports.init = function(user) {
                 targetNode.innerHTML = instance.hb.compile(text)(object);
             }).then(function() {
                 instance.menubar = ec('menu-bar')[0];
-                instance.navbar=ec('navigation-bar')[0];
-                instance.toolbar=ec('tool-area')[0];
+                instance.navbar = ec('navigation-bar')[0];
+                instance.toolbar = ec('tool-area')[0];
                 instance.contextArea = ec('context-area')[0];
                 instance.navIconArea = ec('nav-icon-area')[0];
                 instance.openMenu = ec('open-menu')[0];
@@ -27,14 +27,98 @@ exports.init = function(user) {
             console.log("fetch " + templateURL + " failed")
         })
     }
-    instance.initialize = function() {
-        instance.openMenu.addEventListener('click',instance.openMenuBar);
-        instance.menubar.addEventListener('click',instance.closeMenuBar);
-        instance.openNav.addEventListener('click',instance.openNavBar);
-        instance.navbar.addEventListener('click',instance.closeNavBar);
+    instance.renderRegister = function(templateURL, object, targetNode) {
+        fetch(templateURL).then(function(response) {
+            response.text().then(function(text) {
+                targetNode.innerHTML = instance.hb.compile(text)(object);
+            }).then(function() {
+                ec('register-button')[0].addEventListener('click', function() {
+                    //let obj = {"contact":{"mobileNumber":[1213213,343434],"emailIDs":["xx@yy.com"]},"name":{"firstName":"Dvd","lastName":"Mat","fullName":"Dvd Mat","displayName":"Hello Sir"}};
+                    let user = {};
+                    user.id = en('email-id')[0].value;
+                    user.contact = {};
+                    user.name = {};
+                    user.name.firstName = en('first-name')[0].value;
+                    user.name.lastName = en('last-name')[0].value;
+                    user.name.displayName = user.name.firstName + ' ' + user.name.lastName;
+                    user.name.fullName = user.name.displayName;
+                    user.contact.mobileNumber = [en('mobile-number')[0].value];
+                    user.contact.emailIDs = [user.id];
+                    let password = en('password-one')[0].value;
+                    if (password === en('password-two')[0].value) {
+                        user.password = password;
+                        fetch('http://localhost:7080/user', {
+                            method: "POST",
+                            body: JSON.stringify(user),
+                            headers: new Headers({
+                                'Content-Type': 'application/json'
+                            })
+                        }).then(function() {
+                            console.log('Posted the user');
+                            app.renderLogin();
+                        }).catch(function() {
+                            console.log('Issues');
+                        });
+                    } else {
+                        console.log('password did not match')
+                    }
+                });
+            }).catch(function() {
+                console.log('Unable to read template');
+            })
+        }).catch(function() {
+            console.log("fetch " + templateURL + " failed")
+        })
 
-        instance.openTool.addEventListener('click',instance.openToolBar);
-        instance.toolbar.addEventListener('click',instance.closeToolBar);
+    }
+    instance.renderLogin = function() {
+        fetch('templates/login.html').then(function(res) {
+            res.text().then(function(text) {
+                let template = instance.hb.compile(text);
+                document.getElementById('app-content').innerHTML = template();
+            }).then(function() {
+                ec('login-button')[0].addEventListener('click', function() {
+                    //let obj = {"contact":{"mobileNumber":[1213213,343434],"emailIDs":["xx@yy.com"]},"name":{"firstName":"Dvd","lastName":"Mat","fullName":"Dvd Mat","displayName":"Hello Sir"}};
+                    let user = {};
+                    user.id = en('login-user-name')[0].value;
+                    user.password = en('login-user-password')[0].value
+
+                    fetch('http://localhost:7080/user/login', {
+                        method: "POST",
+                        body: JSON.stringify(user),
+                        headers: new Headers({
+                            'Content-Type': 'application/json'
+                        })
+                    }).then(function(response) {
+                        if (response.status != 200) {
+                            alert(response.statusText);
+                        } else {
+                            response.json().then(function(json) {
+                                app.user = json;
+                                app.render('templates/home.html', json, ei('app-content'));
+                            });
+                        }
+
+                    }).catch(function() {
+                        console.log("Fetching user failed");
+                    });
+                });
+
+                ec('reg-link')[0].addEventListener('click', function() {
+                    window.app.renderRegister('templates/register.html', {}, ei('app-content'));
+
+                });
+            });
+        });
+    }
+    instance.initialize = function() {
+        instance.openMenu.addEventListener('click', instance.openMenuBar);
+        instance.menubar.addEventListener('click', instance.closeMenuBar);
+        instance.openNav.addEventListener('click', instance.openNavBar);
+        instance.navbar.addEventListener('click', instance.closeNavBar);
+
+        instance.openTool.addEventListener('click', instance.openToolBar);
+        instance.toolbar.addEventListener('click', instance.closeToolBar);
     }
     instance.openMenuBar = function() {
         instance.menubar.classList.add('menu-bar-open');
--- index.js
+++ index.js
@@ -9,53 +9,8 @@ window.ei = function(id) {
     return document.getElementById(id);
 }
 
-window.app = require('./app.js');
+window.App = require('./app.js');
 window.addEventListener('load', function(event) {
-    console.log(event);
-    fetch('templates/login.html').then(function(res) {
-        res.text().then(function(text) {
-            let template = hb.compile(text);
-            document.getElementById('app-content').innerHTML = template();
-        }).then(function() {
-            ec('register-button')[0].addEventListener('click', function() {
-                //let obj = {"contact":{"mobileNumber":[1213213,343434],"emailIDs":["xx@yy.com"]},"name":{"firstName":"Dvd","lastName":"Mat","fullName":"Dvd Mat","displayName":"Hello Sir"}};
-                let user = {};
-                user.id = en('email-id')[0].value;
-                user.contact = {};
-                user.name = {};
-                user.name.firstName = en('first-name')[0].value;
-                user.name.lastName = en('last-name')[0].value;
-                user.name.displayName = user.name.firstName + ' ' + user.name.lastName;
-                user.name.fullName = user.name.displayName;
-                user.contact.mobileNumber = [en('mobile-number')[0].value];
-                user.contact.emailIDs = [user.id];
-                fetch('http://localhost:7080/user', {
-                    method: "POST",
-                    body: JSON.stringify(user),
-                    headers: new Headers({
-                        'Content-Type': 'application/json'
-                    })
-                }).then(function() {
-                    console.log('Posted the user');
-                    window.myapp = app.init(user);
-                    myapp.render('templates/home.html', {}, ei('app-content'))
-                }).catch(function() {
-                    console.log('Issues');
-                });
-            });
-
-            ec('login-button')[0].addEventListener('click', function() {
-                //let obj = {"contact":{"mobileNumber":[1213213,343434],"emailIDs":["xx@yy.com"]},"name":{"firstName":"Dvd","lastName":"Mat","fullName":"Dvd Mat","displayName":"Hello Sir"}};
-                let id = en('login-user-name')[0].value;
-                fetch('http://localhost:7080/user/' + id).then(function(response) {
-                    response.json().then(function(json) {
-                        window.myapp = app.init(json);
-                        myapp.render('templates/home.html', json, ei('app-content'));
-                    });
-                }).catch(function(){
-                    console.log("Fetching user failed");
-                });
-            });
-        });
-    });
+    window.app=App.init({});
+    app.renderLogin();
 }, false);
